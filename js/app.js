cb597c19aa601a88c140e8dbcc0b849fdd781aeb
JohnSegar <johnjamessegar@hotmail.co.uk>
2016-03-25 11:45:16 +0000

    Dropping blocks

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
-console.log("hello");
+$(function(){
+  buildGrid();
+  init();
+})
+
+var width  = 5;
+var height = 10;
+var total  = width * height
+
+function buildGrid(){
+  $("body").append("<ul class='grid'></ul>");
+  for (var i = 0; i < total; i++) {
+    $(".grid").append("<li></li>")
+  }
+}
+
+function init(){
+  var $lis = $("li"); // HTMLCollection, like an Array
+  drop();
+}
+
+function drop(){
+  var $lis = $("li");
+
+  // Pick one of the top row
+  var nextIndex = Math.floor(Math.random()*width); // From 0 to width
+  var prevIndex;
+
+
+  // Create a setInterval to run repeatedly
+  var dropInterval = setInterval(function(){
+    // Choose the next li
+    var prevLi = $lis[prevIndex]
+    var nextLi = $lis[nextIndex];
+    
+    // Colour that li
+    if (prevLi) $(prevLi).removeClass("red");
+    $(nextLi).addClass("red")
+
+    // Assign the previous and next indexes
+    prevIndex = nextIndex;
+    nextIndex = nextIndex + width;
+
+    // If it hits the bottom, then stop
+    if (nextIndex > total-1) {
+      drop();
+      return clearInterval(dropInterval)
+    };
+  }, 200)
+ 
+}
