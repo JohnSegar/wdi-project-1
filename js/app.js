90e23810ed0151878bd3a21ce83deff5a636c434
JohnSegar <johnjamessegar@hotmail.co.uk>
2016-03-28 09:18:07 +0100

    Blocks dropping, stopping and colour changing

--- js/app.js
+++ js/app.js
@@ -8,9 +8,6 @@ var height = 20;
 var total  = width * height
 var block  = document.getElementsByClassName("color");
 var moves  = []
-// var colors = ['red', 'green', 'blue', 'yellow'];
-// var random_color = colors[Math.floor(Math.random() * colors.length)];
-// document.getElementById('title').style.color = random_color;
 
 function buildGrid(){
   $("body").append("<ul class='grid'></ul>");
@@ -30,38 +27,47 @@ function checkForFullRow(){
 }
 
 function drop(){
+  // Get all lis
   var $lis = $("li");
 
   // Pick one of the top row
   var nextIndex = Math.floor(Math.random()*width); // From 0 to width
+  // Choose the previous row (at the start it's undefined)
   var prevIndex;
 
+  var colors = ['red', 'green', 'blue', 'yellow'];
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
   // Create a setInterval to run repeatedly
   var dropInterval = setInterval(function(){
     // Choose the next li
     var prevLi = $lis[prevIndex]
-    var nextLi = $lis[nextIndex];
+    var nextLi = $lis[nextIndex];  
     
-    // If next li is red, then stop and drop another
-    if ($(nextLi).hasClass("color")) {
+    // If next li has a class of `.played`, then stop and drop another
+    if ($(nextLi).hasClass("played"))  {
       // Add moves to an array of moves
       moves.push(prevIndex);
+      randomColor = colors[Math.floor(Math.random() * colors.length)];
       checkForFullRow()
       drop();
       return clearInterval(dropInterval)
     }
 
     // Colour that li
-    if (prevLi) $(prevLi).removeClass("color");
-    $(nextLi).addClass("color")
+    $(nextLi)
+      .addClass(randomColor)
+      .addClass("played");
+    
+    if (prevLi) {
+      $(prevLi)
+      .removeClass(randomColor)
+      .removeClass("played");
+    }
 
     // Assign the previous and next indexes
     prevIndex = nextIndex;
     nextIndex = nextIndex + width;
-
-    // If it hits the bottom, then stop
-    // var block = document.getElementsByClassName("color");
 
     if (nextIndex > total-1) {
       // Add moves to an array of moves
